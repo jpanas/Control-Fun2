1ea0c97bdd059d14ed8896d73957dc8efeec1f74
Jason <jpanas1@wc320-mac11.local>
2016-01-28 11:05:01 -0700

    Up to page 121

--- Control Fun2/ViewController.swift
+++ Control Fun2/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var numberField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,16 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func textFieldDoneEditing(sender: UITextField)
+    {
+        sender.resignFirstResponder()
+    }
+    
+    @IBAction func onTapGestureRecognized(sender: AnyObject)
+    {
+        nameField.resignFirstResponder()
+        numberField.resignFirstResponder()
+    }
 }
 
